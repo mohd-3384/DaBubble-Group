dcb81f7d75c34372c78dd0669ccd9e0d24bb89da
mohd-3384 <mohd3384@hotmail.com>
2025-10-25 12:00:01 +0200

    ix: corrected public path mapping and font URLs in styles.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
-<<<<<<< HEAD
-
-export const routes: Routes = [];
-=======
 export const routes: Routes = [
-    { path: '', component: LoginComponent},
+  { path: '', component: LoginComponent },
 ];
->>>>>>> 7bea093cec75fa2f127675bcac304206f3899462
