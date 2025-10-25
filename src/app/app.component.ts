ad10c757b0e5a69fa2525d8ab7cb8285c8f86761
David Kolosza <david.kolosza@gmx.de>
2025-10-25 10:30:27 +0200

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { ChatComponent } from './chat/chat.component';
+import { ThreadComponent } from './thread/thread.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RouterLink, ChannelsComponent, ChatComponent, ThreadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
