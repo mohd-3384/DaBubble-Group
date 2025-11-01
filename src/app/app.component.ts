2c7a5529ed7a9c62f0a62f547208ec517eaa7841
David Kolosza <david.kolosza@gmx.de>
2025-11-01 12:23:24 +0100

    routes-fix-issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule} from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { ChannelsComponent } from './channels/channels.component';
 import { ChatComponent } from './chat/chat.component';
 import { ThreadComponent } from './thread/thread.component';
-// import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ChannelsComponent, ChatComponent, ThreadComponent],
+  imports: [HeaderComponent, RouterOutlet, ChannelsComponent, ChatComponent, ThreadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dabubble';
+
 }
