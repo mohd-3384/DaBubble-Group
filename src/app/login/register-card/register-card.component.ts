25d6040912a336cfaa0a5c6298f0274d26b46d29
Eduard Fray <eduard.fray@hotmail.de>
2025-11-14 16:02:30 +0100

    Form for Register Component

--- src/app/login/register-card/register-card.component.ts
+++ src/app/login/register-card/register-card.component.ts
@@ -7,6 +7,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-card',
@@ -16,12 +18,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatDividerModule,
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './register-card.component.html',
   styleUrls: ['./register-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterCardComponent {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z -]+$')] ],
+    email: ['', [Validators.required, Validators.email] ],
+    password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[A-Z])(?=.*[!@#$&*]).{6,}$')] ],
+    terms: [false, [Validators.requiredTrue] ],
+  });
+  }
+
+  onsubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted!', this.form.value);
+    }
+  }
 
 }
