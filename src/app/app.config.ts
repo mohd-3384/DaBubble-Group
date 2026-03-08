a9a058d4a72b90757b175bfdd7f588025e9bc982
mohd-3384 <mohd3384@hotmail.com>
2026-03-08 10:31:31 +0100

    Fix: Update layout limits, header/search UX, and env config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,15 +9,7 @@ import { FirebaseErrorHandler } from './services/firebase-error-handler.service'
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAgc0UhrDkD0iQWuSgsuY6t-7V8oZ5zVAc',
-  authDomain: 'dabubble-group.firebaseapp.com',
-  projectId: 'dabubble-group',
-  storageBucket: 'dabubble-group.firebasestorage.app',
-  messagingSenderId: '940020886287',
-  appId: '1:940020886287:web:d5d8546fe0a9bb6bdd6c7a',
-};
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,7 +19,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: ErrorHandler, useClass: FirebaseErrorHandler },
 
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
