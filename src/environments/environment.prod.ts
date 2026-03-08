54504107e1750b7163f15e6334358f8e8305dfcd
mohd-3384 <mohd3384@hotmail.com>
2026-03-08 10:59:12 +0100

    Fix: Update layout limits, header/search UX, and env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyAgc0UhrDkD0iQWuSgsuY6t-7V8oZ5zVAc',
+    authDomain: 'dabubble-group.firebaseapp.com',
+    projectId: 'dabubble-group',
+    storageBucket: 'dabubble-group.firebasestorage.app',
+    messagingSenderId: '940020886287',
+    appId: '1:940020886287:web:d5d8546fe0a9bb6bdd6c7a',
   },
 };
